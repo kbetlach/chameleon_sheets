2efc354db6ff52c3739325dcd6e7c6a850c69e74
kbetlach <kbetlach@gmail.com>
2020-02-23 16:06:23 -0600

    header and footer components made. login component filled out as boilerplate. css file also created

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import '../../assets/style.css';
 
-function Index() {
+function Login() {
     return (
     <div className = "container">
         <div className = "jumbotron">
-            <p>CHAMELEON SHEETS!</p>
+            <h1>Welcome to Chameleon Sheets</h1>
+
+            <br />
+
+            <p>Returning user? Please login here (Button)</p>
+
+            <br />
+
+            <p>New to working with us? Signup here (Button)</p>
+
+            <br />
+
+            <p>(Perhaps a quick 3-4 sentance plug about who we are and what we do would go here, and a picture up here ^)</p>
         </div>
     </div>
     )
 }
 
-export default Index;
+export default Login;

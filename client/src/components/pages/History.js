00854a9dd6d2dd666c2871181aab6689f447cd9f
kbetlach <kbetlach@gmail.com>
2020-02-26 13:57:28 -0600

    linked allll pages. added footer to app.js, imported header to all pages. folder restructure

---
+++ client/src/components/pages/History.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../assets/css/style.css';
+import Header from "../Header"
+
+function History() {
+    return (
+        <Header />
+    )
+}
+
+export default History;

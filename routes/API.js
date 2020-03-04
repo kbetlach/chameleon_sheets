c319eec587a28a5a720b78adf9d706133df765e3
Ben Honken <honkenb@gmail.com>
2020-03-04 14:38:06 -0600

    updated API routes to properly connect to database

--- routes/API.js
+++ routes/API.js
@@ -7,9 +7,9 @@ module.exports = function(app) {
     });
 
     app.post("/api/user/", function(req, res) {
-        db.Users.create(req.body)
-            .then(function() {
-                res.redirect(307, "/api/login");
+        db.User.create(req.body)
+            .then(function(user) {
+                res.json(user);
             })
             .catch(function(err) {
                 res.status(401).json(err);
@@ -25,22 +25,22 @@ module.exports = function(app) {
     });
    // GET ROUTES
 app.get("/api/user/", function(req, res) {
-    db.Users.find({id: req.user.id}).then(function(dbUsers) {
+    db.User.find({id: req.user.id}).then(function(dbUsers) {
         res.json(dbUsers);
     });
 });
 app.get("/api/get_user_by_id/:id", function(req, res) {
-    db.Users.find({id: req.params.id}).then(function(dbUsers) {
+    db.User.find({id: req.params.id}).then(function(dbUsers) {
         res.json(dbUsers);
     });
 })
 app.get("/api/get_logs/", function(req, res) {
-    db.Logs.find({}).then(function(dbLogs) {
+    db.Log.find({}).then(function(dbLogs) {
         res.json(dbLogs);
     });
 });
 app.get("/api/get_logs_by_id/:id", function(req, res) {
-    db.Logs.find({}).then(function(dbLogs) {
+    db.Log.find({}).then(function(dbLogs) {
         res.json(dbLogs);
     });
 });
@@ -48,7 +48,7 @@ app.post("/api/new_student", function(req, res) {
     var log = req.body;
     log.tutor_user_id = req.user.id;
     log.tutor_name = req.user.first_name + " " + req.user.last_name;
-    db.Logs.create(log).then(function(dbLogs) {
+    db.Log.create(log).then(function(dbLogs) {
         res.json(dbLogs);
     });
 });
@@ -56,10 +56,15 @@ app.post("/api/new_log", function(req, res) {
     var log = req.body;
     log.tutor_user_id = req.user.id;
     log.tutor_name = req.user.first_name + " " + req.user.last_name;
-    db.Logs.create(log).then(function(dbLogs) {
+    db.Log.create(log).then(function(dbLogs) {
         res.json(dbLogs);
     });
 });
+app.put("/api/update_password/", function(req, res) {
+    db.User.findOneAndUpdate({email: req.body.email, password: { $exists: false}}, {password: req.body.password}).then(function(result) {
+        return res.json(result);
+    });
+})
 app.put("/api/update_user/", function(req, res) {
     let updateUser = {
         id: req.body.id,
@@ -67,10 +72,9 @@ app.put("/api/update_user/", function(req, res) {
         last_name: req.body.last_name,
         email: req.body.email,
         role: req.body.role,
-        hours: req.body.hours,
-        tutor_id: req.body.tutor_id
+        school: req.body.school
     }
-    db.Users.findOneAndUpdate({id: req.body.id}, updateUser).then(function(result) {
+    db.User.findOneAndUpdate({id: req.body.id}, updateUser).then(function(result) {
         return res.json(result);
     });
 })

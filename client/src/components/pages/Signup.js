85c32434c1ac6a97f914768536557a53df718894
kbetlach <kbetlach@gmail.com>
2020-03-04 15:17:23 -0600

    added logout button / route to header. signup page now has state/onchange for password creation

--- client/src/components/pages/Signup.js
+++ client/src/components/pages/Signup.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from "react";
 import '../../assets/css/style.css';
+import API from '../../utils/API';
 
 function Signup() {
+
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    API.createPassword({
+        email: email,
+        password: password
+      }) 
+
     return (
         <div>
             <div className="container w3-container w3-center w3-animate-opacity">
@@ -9,11 +19,11 @@ function Signup() {
                     <h2>Welcome to Chameleon Sheets. Please confirm your email and choose a password for access.</h2>
                     <div className="form-group">
                         <label for="emailInput">Confirm Email Address</label>
-                        <input className="form-control" id="email-signup" placeholder="Email" type="email"></input>
+                        <input onChange={e => setEmail(e.target.value)} className="form-control" id="email-signup" placeholder="Email" type="email"></input>
                     </div>
                     <div className="form-group">
                         <label for="passwordInput">Password</label>
-                        <input className="form-control" id="password-signup" placeholder="Password" type="password"></input>
+                        <input onChange={e => setPassword(e.target.value)} className="form-control" id="password-signup" placeholder="Password" type="password"></input>
                     </div>
                     <input style={{ marginTop: "20px", backgroundColor: "darkslategray", color: "white", borderRadius: "6px", border: ".5px solid white" }} type="submit"></input>
                 </div>

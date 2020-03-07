a9c4fb862f623298296da615141de5433176e3c6
kbetlach <kbetlach@gmail.com>
2020-03-07 14:06:21 -0600

    added links to new button components

--- client/src/components/Buttons/HistoryButton/HistoryButton.js
+++ client/src/components/Buttons/HistoryButton/HistoryButton.js
@@ -1,27 +1,33 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function HistoryButton() {
 
     return (
-<div>
-<button type="button" 
-style={
-    {
-        width: "100px",
-        height: "50px",
-        fontSize: "24px",
-        marginTop: "20px",
-        backgroundColor: "white",
-        color: "darkslategray",
-        borderRadius: "6px",
-        border: ".5px solid white",
-        padding: 5
-    }
-}>
-History
-    </button>
-</div>
+        <div>
+            <Link to="/admin"
+                className={
+                    window.location.pathname === "/history"
+            }>
+                <button type="button"
+                    style={
+                        {
+                            width: "100px",
+                            height: "50px",
+                            fontSize: "24px",
+                            marginTop: "20px",
+                            backgroundColor: "white",
+                            color: "darkslategray",
+                            borderRadius: "6px",
+                            border: ".5px solid white",
+                            padding: 5
+                        }
+                }>
+                    History
+                </button>
+            </Link>
+        </div>
     )
 }
 
-export default HistoryButton
+export default HistoryButton;

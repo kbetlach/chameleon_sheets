d71a57187fe1bae2a15c9afb0ac0173c895e2e3c
kbetlach <kbetlach@gmail.com>
2020-03-08 10:02:25 -0500

    created view all users page and associated button component, minor style tweaks like background image

---
+++ client/src/components/Buttons/ViewAllUsersButton/ViewAllUsersButton.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+function ViewAllUsersButton() {
+
+    return (
+        <div>
+            <Link to="/users"
+                className={
+                    window.location.pathname === "/users"
+            }>
+                <button type="button"
+                    style={
+                        {
+                            width: "100px",
+                            height: "50px",
+                            fontSize: "24px",
+                            marginTop: "20px",
+                            backgroundColor: "white",
+                            color: "darkslategray",
+                            borderRadius: "6px",
+                            border: ".5px solid white",
+                            padding: 5
+                        }
+                }>
+                    View Users
+                </button>
+            </Link>
+        </div>
+    )
+}
+
+export default ViewAllUsersButton;

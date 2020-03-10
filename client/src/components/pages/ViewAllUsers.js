3c47cac2f3bdc003ac5e520f9633d0c24eb4b704
Ben Honken <honkenb@gmail.com>
2020-03-10 15:35:36 -0500

    added lockout based on login

--- client/src/components/pages/ViewAllUsers.js
+++ client/src/components/pages/ViewAllUsers.js
@@ -1,23 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../Header/Header";
 import './pages.css';
+import API from '../../utils/API';
 
 function ViewAll() {
+    const [isAdmin, setIsAdmin] = useState();
+    const [teachers, setTeachers] = useState();
+    const [students, setStudents] = useState();
+    const [guardians, setGuardians] = useState();
+    async function checkYourself(){
+        let userPlaceholder = await API.getSelf();
+        if(userPlaceholder && (userPlaceholder.data.role === "Admin")){
+            setIsAdmin(true)
+        }
+        else{
+            setIsAdmin(false)
+        }
+    }
+    useEffect(() => {
+        checkYourself();
+      },[])
+    async function getUsers(){
+        let teacherPlaceholder = await API.getTeachers();
+        if(teacherPlaceholder && teacherPlaceholder.data){
+            setTeachers(teacherPlaceholder.data)
+        }
+        let guardianPlaceholder = await API.getGuardians();
+        if(guardianPlaceholder && guardianPlaceholder.data){
+            setGuardians(guardianPlaceholder.data)
+        }
+        let studentPlaceholder = await API.getStudents();
+        if(studentPlaceholder && studentPlaceholder.data){
+            setStudents(studentPlaceholder.data)
+        }
+        console.log(teacherPlaceholder.data)
+        console.log(guardianPlaceholder.data)
+        console.log(studentPlaceholder.data)
+    }
+    useEffect(() => {
+        getUsers();
+      },[])
     return (
         <div>
-        <Header />
-        <div className="container container w3-container w3-center w3-animate-opacity" style={{ margin: "auto", width: "75%" }}>
-            <div className="jumbotron">
-                <h1 className="text-center" style={{color: "white", fontSize: "42px", marginBottom: "25px"}}>All Users</h1>
+            {(isAdmin) ? (
+                <div>
+                    <Header />
+                    <div className="container container w3-container w3-center w3-animate-opacity" style={{ margin: "auto", width: "75%" }}>
+                        <div className="jumbotron">
+                            <h1 className="text-center" style={{color: "white", fontSize: "42px", marginBottom: "25px"}}>All Users</h1>
 
-                <div className="row">
-                    <div className="col-md-4" style={{color: "white", fontSize: "28px", textDecoration: "underline"}}>Teachers</div>
-                    <div className="col-md-4" style={{color: "white", fontSize: "28px", textDecoration: "underline"}}>Students</div>
-                    <div className="col-md-4" style={{color: "white", fontSize: "28px", textDecoration: "underline"}}>Guardians</div>
+                            <div className="row">
+                                <div className="col-md-4" style={{color: "white", fontSize: "28px", textDecoration: "underline"}}>Teachers</div>
+                                <div className="col-md-4" style={{color: "white", fontSize: "28px", textDecoration: "underline"}}>Students</div>
+                                <div className="col-md-4" style={{color: "white", fontSize: "28px", textDecoration: "underline"}}>Guardians</div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>) : (<div className="jumbotron">
+                <h1 className="text-center" style={{color: "white", fontSize: "42px", marginBottom: "25px"}}>You must be an admin to view this page</h1>
                 </div>
-
-            </div>
-            </div>
+)}
         </div>
     )
     }

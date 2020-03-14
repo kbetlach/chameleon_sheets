09042bf05187a2ac42c15a05f747c8f17d1cdecb
Ben Honken <honkenb@gmail.com>
2020-03-14 00:02:41 -0500

    guardians only see their students, and their data populates correctly.

--- client/src/components/Columns/TabWrapper.js
+++ client/src/components/Columns/TabWrapper.js
@@ -1,11 +1,16 @@
 import React from "react";
 import StudentTabs from "./StudentTabs";
+import StudentTabsDisabled from "./StudentTabsDisabled";
 // import DateTab from "./DateTab";
 
 function TabWrapper(props){
     return (
         <div className="tab-wrapper">
-            <StudentTabs setStudentId={props.setStudentId} setActiveTab={props.setActiveTab} setDate={props.setDate}/>
+            {props.disabled ? (
+                <StudentTabsDisabled setStudentId={props.setStudentId} setActiveTab={props.setActiveTab} setDate={props.setDate}/>
+            ):(
+                <StudentTabs setStudentId={props.setStudentId} setActiveTab={props.setActiveTab} setDate={props.setDate}/>
+            )}
         </div>
     )
 }
